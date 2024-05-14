dad17112f8e06da40e197324d73271b71cf013ba
jihyeon2434 <ubis970420@gmail.com>
2024-05-14 09:29:16 +0900

    시크릿 키가 존재해야함

--- src/main/java/com/koreait/jwt_2024_05/Jwt202405Application.java
+++ src/main/java/com/koreait/jwt_2024_05/Jwt202405Application.java
@@ -1,13 +1,13 @@
-package com.koreait.jmt_2024_05;
+package com.koreait.jwt_2024_05;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Jmt202405Application {
+public class Jwt202405Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Jmt202405Application.class, args);
+        SpringApplication.run(Jwt202405Application.class, args);
     }
 
 }
