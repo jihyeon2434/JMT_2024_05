7a00b944000c5f089a9a471cf4e1f774afaf8b42
jihyeon2434 <ubis970420@gmail.com>
2024-05-14 09:19:00 +0900

    프로젝트 세팅

---
+++ src/main/java/com/koreait/jmt_2024_05/Jmt202405Application.java
@@ -0,0 +1,13 @@
+package com.koreait.jmt_2024_05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jmt202405Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jmt202405Application.class, args);
+    }
+
+}
